3739abf02e30b8760c02ec3bd81f1d3b6a8ece6b
kanyok <134153117+nullvoid-ky@users.noreply.github.com>
2024-07-15 16:25:00 +0700

    feat: add SearchBar at HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Footer from "../utils/Footer";
 import Background from "../utils/Background";
 import Banner from "../home/Banner";
 import ScrollContent from "../home/ScrollContent";
+import SearchBar from "../boardgame/SearchBar";
 export default function HomePage() {
   
   return (
@@ -12,6 +13,7 @@ export default function HomePage() {
         <Nav />
         <div className=" min-h-screen  flex flex-col justify-center pt-12 sm:pt-36  p-0 sm:p-20 ">
           <Banner />
+          <SearchBar/>
           <div className="flex p-4 flex-col justify-center sm:space-y-8">
             <ScrollContent Category="Psychology"/>
             <ScrollContent Category="Hide n Seek"/>
